fa5504dcadd25eecc51659b48d477be10ad8ff26
farihanoor <Fariha041295**>
2021-08-09 13:48:33 +0600

    Singleton class setup

---
+++ app/src/main/java/com/example/trivia/controller/AppController.java
@@ -0,0 +1,65 @@
+package com.example.trivia.controller;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+    private static AppController instance;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+    private static Context ctx;
+
+    private AppController(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    @Nullable
+                    private final LruCache<String, Bitmap>
+                        cache = new LruCache<String, Bitmap>(20);
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    // this is where Singleton is created
+    public static synchronized AppController getInstance(Context context) {
+        if (instance == null) {
+            instance = new AppController(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if(requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+
+}
