01adbb0ac6d9b4a705285d659dd70276dffda976
farihanoor <Fariha041295**>
2021-08-10 14:16:15 +0600

    Interface Completed

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -3,13 +3,38 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.trivia.controller.AppController;
+import com.example.trivia.data.AnswerListAsyncResponse;
+import com.example.trivia.data.Repository;
+import com.example.trivia.model.Question;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Question> questions = new Repository().getQuestion(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+                Log.d("Main", "onCreate: " + questionArrayList);
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/trivia/controller/AppController.java
+++ app/src/main/java/com/example/trivia/controller/AppController.java
@@ -15,41 +15,16 @@ import com.android.volley.toolbox.Volley;
 public class AppController extends Application {
     private static AppController instance;
     private RequestQueue requestQueue;
-    private ImageLoader imageLoader;
-    private static Context ctx;
 
-    private AppController(Context context) {
-        ctx = context;
-        requestQueue = getRequestQueue();
-
-        imageLoader = new ImageLoader(requestQueue,
-                new ImageLoader.ImageCache() {
-                    @Nullable
-                    private final LruCache<String, Bitmap>
-                        cache = new LruCache<String, Bitmap>(20);
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
-    }
 
     // this is where Singleton is created
-    public static synchronized AppController getInstance(Context context) {
-        if (instance == null) {
-            instance = new AppController(context);
-        }
+    public static synchronized AppController getInstance() {
         return instance;
     }
 
     public RequestQueue getRequestQueue() {
         if(requestQueue == null) {
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
         return requestQueue;
     }
@@ -58,8 +33,9 @@ public class AppController extends Application {
         getRequestQueue().add(req);
     }
 
-    public ImageLoader getImageLoader() {
-        return imageLoader;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
     }
-
 }
